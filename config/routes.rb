fe7b5dd620d8e015a1e95b558aefa322ad7294c8
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-09-08 12:02:32 +0200

    added sum for google login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
 end
