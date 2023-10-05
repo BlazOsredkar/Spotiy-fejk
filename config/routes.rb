bace42f94f3df844b8d7a551dfcfe9b6fec7980b
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-10-05 10:06:12 +0200

    added user show

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   delete 'playlists/:playlist_id/remove_song/:song_id', to: 'playlists#remove_from_playlist', as: :remove_from_playlist
   resources :albums
   resources :playlists
-  root 'home#home'
+  root 'pages#home'
   resources :users, only: [:show]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
