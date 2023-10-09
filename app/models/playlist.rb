2fa92d3935b4a6851124e3ab561e4c0893a7e3cf
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-10-09 09:20:54 +0200

    update

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,6 +3,10 @@ class Playlist < ApplicationRecord
     has_many :playlist_songs
     has_many :songs, through: :playlist_songs
 
+    def songs_by_position
+        self.songs.order('playlists_songs.position')
+    end
+
 
 
 
